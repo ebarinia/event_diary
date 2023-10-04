5c37c52f58cb0c39798d377c7cbecaae7b05a999
Emmanuel Debion <emmanueldebion@Emmanuels-MacBook-Pro.local>
2023-10-04 12:21:14 +0100

    merged to main branch

--- app/components/EventDiaryContainer.jsx
+++ app/components/EventDiaryContainer.jsx
@@ -12,10 +12,8 @@ const EventDiaryContainer = () => {
   const [events, setEvents] = useState ([])
   const [userEvents, setUserEvents] = useState ([])
   const [calendarEvent, setCalendarEvent] = useState({})
-  const page = "allevents"
-  // const [selectedEvent, setSelectedEvent] = useState(null)
   const [isLoading, setLoading] = useState(true)
-
+  const page = "allevents"
   
   useEffect(() => {
     EventDataService.getAllEvents()
@@ -50,24 +48,21 @@ const EventDiaryContainer = () => {
   else {
   return (
     <>
-
     <Header/>
     <header className="h-[95vh]  flex flex-col items-center justify-center header">
       <div className='mx-auto max-w-[75%]'>
       <h1 class="mb-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-600 md:text-5xl lg:text-6xl">Event Diary</h1>
       <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 text-center">Your personal booking and diary system for your events, all in one place. Discover events in your area, book or shortlist them for later</p>
-
+      </div>
+    </header>
+    
       <RecommendedEvent events={events}/>
-      <div className='flex mx-auto max-w-[75%] mt-[10%] bg-slate-200 rounded-xl'>
-      <div className='w-1/2'>
-        <EventCalendar userEvents={userEvents} updateCalendarDetail = {updateCalendarDetail}/>
-      </div>
-      <div className='w-1/2 mt-[5%]'>
-        <h2 className='text-2xl'>Your next event</h2>
-        <EventCalendarDetail userEvents={userEvents} calendarEvent={calendarEvent} updateBooking={updateBooking}/>
-
-      </div>
-  
+      <div className='bg-gray-700 mb-[10%]'>
+        <div className='flex mx-auto max-w-[75%] h-[50vh]'>
+          <div className='w-1/2'>
+            <EventCalendar userEvents={userEvents} updateCalendarDetail = {updateCalendarDetail}/>
+          </div>
+          <div className='w-1/2 mt-[5%]'>
             <EventCalendarDetail userEvents={userEvents} calendarEvent={calendarEvent}/>
           </div>
         </div>
